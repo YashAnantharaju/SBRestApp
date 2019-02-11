c04064673adece9e38b0ad8658aace25a48e6db0
kranthikumar <kranthi.k@anblicks.com>
2019-02-12 04:47:05 +0530

    3rd commit

--- SBRest/src/main/java/com/grootan/v1/SBRest/constants/SBRestConstants.java
+++ SBRest/src/main/java/com/grootan/v1/SBRest/constants/SBRestConstants.java
@@ -16,23 +16,24 @@ public class SBRestConstants {
 
 	
 	public static String[][] QuestionsAndAnswers = {
-			{"what is your name","Kranthi"},
-			{"what is your name","Kranthi"},
-			{"what is your name","Kranthi"},
-			{"what is your name","Kranthi"},
-			{"what is your name","Kranthi"},
-			{"what is your name","Kranthi"},
-			{"what is your name","Kranthi"},
-			{"what is your name","Kranthi"},
-			{"what is your name","Kranthi"},
-			{"what is your name","Kranthi"},
-			{"what is your name","Kranthi"},
-			{"what is your name","Kranthi"},
-			{"what is your name","Kranthi"},
-			{"what is your name","Kranthi"},
-			{"what is your name","Kranthi"},
-			{"what is your name","Kranthi"},
-			{"what is your name","Kranthi"}
+			{"One Plus One ?","Two"},
+			{"Twenty Multiples Ten ?","Two Hundred"},
+			{"Zero Multiples One ?","Zero"},
+			{"Hundred Multiples One ?","Hundred"},
+			{"Zero Plus Zero","Zero"},
+			{"One Divides One","One"},
+			{"Zero Divides One","Zero"},
+			{"One Plus Two ?","Three"},
+			{"Twenty Multiples Twenty ?","Four Hundred"},
+			{"Zero Multiples Zero ?","Zero"},
+			{"Hundred Multiples Hundred ?","One Thousand"},
+			{"Twenty Plus Zero","Twenty"},
+			{"Two Divides Two","One"},
+			{"Four Divides Two","Two"},
+			{"Zero Plus Seven","Zero"},
+			{"Seven Divides One","Seven"},
+			{"Four Divides Four","One"},
+			
       };
 
 }
