95f92f6e09a89b738b634b92ca6f1c4b70373de1
kranthikumar <kranthi.k@anblicks.com>
2019-02-12 03:24:38 +0530

    first commit

---
+++ SBRest/src/main/java/com/grootan/v1/SBRest/constants/SBRestConstants.java
@@ -0,0 +1,38 @@
+package com.grootan.v1.SBRest.constants;
+
+
+/**
+ * 
+ * @author kranthi.kumar
+ *
+ */
+
+public class SBRestConstants {
+	
+	public static final String getGridKey = "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";
+	public static String EMPTY_STRING = "";
+	public static Boolean TRUE = true;
+	public static Boolean FALSE = false;
+
+	
+	public static String[][] QuestionsAndAnswers = {
+			{"what is your name","Kranthi"},
+			{"what is your name","Kranthi"},
+			{"what is your name","Kranthi"},
+			{"what is your name","Kranthi"},
+			{"what is your name","Kranthi"},
+			{"what is your name","Kranthi"},
+			{"what is your name","Kranthi"},
+			{"what is your name","Kranthi"},
+			{"what is your name","Kranthi"},
+			{"what is your name","Kranthi"},
+			{"what is your name","Kranthi"},
+			{"what is your name","Kranthi"},
+			{"what is your name","Kranthi"},
+			{"what is your name","Kranthi"},
+			{"what is your name","Kranthi"},
+			{"what is your name","Kranthi"},
+			{"what is your name","Kranthi"}
+      };
+
+}
